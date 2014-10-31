d3cc2b530d99ba911b9098156ac990afe3623b8a
Joe Kutner <jpkutner@gmail.com>
2014-10-31 08:15:57 -0500

    removed sbt stuff to make it pure java

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 JDK_HOME=`pwd`
 
-JVM_COMMON_BUILDPACK=http://lang-jvm.s3.amazonaws.com/jvm-buildpack-common-v2.tar.gz
+JVM_COMMON_BUILDPACK=http://lang-jvm.s3.amazonaws.com/jvm-buildpack-common-v3.tar.gz
 mkdir -p /tmp/jvm-common
 curl --silent --location $JVM_COMMON_BUILDPACK | tar xzm -C /tmp/jvm-common
 . /tmp/jvm-common/bin/util
@@ -14,18 +14,3 @@ echo -n " ---> Installing OpenJDK ${javaVersion}..."
 install_java ${JDK_HOME} ${javaVersion}
 jdk_overlay ${JDK_HOME}
 echo " done"
-
-
-SBT_VERSION="0.13.5"
-SBT_JAR="sbt-launch.jar"
-SBT_URL="http://typesafe.artifactoryonline.com/typesafe/ivy-releases/org.scala-sbt/sbt-launch/0.13.5/$SBT_JAR"
-SBT_BINDIR=.sbt_home
-mkdir -p "$SBT_BINDIR"
-cd "$SBT_BINDIR"
-
-## clean up any old versions
-/bin/rm -f sbt sbt-launch-*.jar || true
-
-echo -n " ---> Downloading SBT..."
-curl --silent --max-time 60 -O --location $SBT_URL --fail || error "Failed to download $SBT_JAR"
-echo "done"
